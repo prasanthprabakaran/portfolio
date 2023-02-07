f4643b9110b8e476cb5e00aec479f6c587309603
prasanthprabakaran <iprasanthpraba@gmail.com>
2023-02-07 12:21:33 +0530

    contact

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
           <Route path="/projects" element={<Projects/>} />
           <Route path="/project/:id" element={<ProjectDisplay/>} />
           <Route path='/experience' element={<Experience/>} />
-          <Route path='contact' element={<Contact/>} />
+          <Route path='/contact' element={<Contact/>} />
         </Routes>
         <Footer/>
       </Router>
